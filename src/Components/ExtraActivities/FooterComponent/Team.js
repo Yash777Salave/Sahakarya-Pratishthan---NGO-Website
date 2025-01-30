8b5be45e1ab92c0e25fa7390f1d20203e013fa4c
Yash777Salave <yashsalave777@rediffmail.com>
2025-01-30 10:48:43 +0530

    updated Team.js componant where added new style to team members's informations

--- src/Components/ExtraActivities/FooterComponent/Team.js
+++ src/Components/ExtraActivities/FooterComponent/Team.js
@@ -2,8 +2,29 @@ import Navbar from "../../Navbar";
 import PickMeal from "../../../Assets/pick-meals-image.png";
 import ChooseMeals from "../../../Assets/choose-image.png";
 import DeliveryMeals from "../../../Assets/delivery-image.png";
-import Tejaswini from "../../../Assets/Team/Tejaswini.jpg";
+import Tejaswini2 from "../../../Assets/Team/Tejaswini2.jpg";
 import Vaishali from "../../../Assets/Team/Vaishali.jpg";
+import akshayblacky from "../../../Assets/Team/akshayblacky.jpg";
+import akshaypatil from "../../../Assets/Team/akshaypatil.jpg";
+import akshayT from "../../../Assets/Team/akshayT.jpg";
+import amol from "../../../Assets/Team/amol.jpg";
+import bhupesh from "../../../Assets/Team/bhupesh.jpg";
+import chiku from "../../../Assets/Team/chiku.jpg";
+import div from "../../../Assets/Team/div.jpg";
+import gym from "../../../Assets/Team/gym.jpg";
+import Himanshu from "../../../Assets/Team/Himanshu.jpg";
+import jayu2 from "../../../Assets/Team/jayu2.jpg";
+import kalyani2 from "../../../Assets/Team/kalyani2.jpg";
+import mayur from "../../../Assets/Team/mayur.jpg";
+import om from "../../../Assets/Team/om.jpg";
+import Prasad from "../../../Assets/Team/Prasad.jpg";
+import pravin2 from "../../../Assets/Team/pravin2.jpg";
+import rana from "../../../Assets/Team/rana.jpg";
+import sahil from "../../../Assets/Team/sahil.jpg";
+import tejas2 from "../../../Assets/Team/tejas2.jpg";
+import tejaspadvi from "../../../Assets/Team/tejaspadvi.jpg";
+import yashBaba from "../../../Assets/Team/yashBaba.jpg";
+import parth from "../../../Assets/Team/parth.jpg";
 import CountUp from "react-countup";
 import { useNavigate } from "react-router-dom";
 import Footer from "../../Footer";
@@ -12,123 +33,123 @@ const Team = () => {
 
   const workInfoData = [
     {
-      image: DeliveryMeals,
+      image: pravin2,
       title: "Pravin Nikum",
       text: "President",
       // Profile: "SoftWare Developer",
     },
     {
-      image: DeliveryMeals,
+      image: tejas2,
       title: "Teajs Thakur",
       text: "Vice-President",
     },
     {
-      image: DeliveryMeals,
+      image: mayur,
       title: "Mayur Koli",
       text: "Secretary",
     },
 
     {
-      image: DeliveryMeals,
+      image: yashBaba,
       title: "Yash Salave",
       text: "Vice-Secretary",
     },
 
     {
-      image: DeliveryMeals,
+      image: Prasad,
       title: "Prasad Wani",
       text: "Treasurer",
     },
 
     {
-      image: DeliveryMeals,
+      image: rana,
       title: "RudrapratapSing Ahirrao",
       text: "Working President",
     },
 
     {
-      image: DeliveryMeals,
+      image: chiku,
       title: "Khushal Wagh",
       text: "Research Analyst",
     },
 
     {
-      image: DeliveryMeals,
+      image: om,
       title: "Rohit Aghav",
       text: "District Manager (Dhule)",
     },
 
     {
-      image: DeliveryMeals,
+      image: jayu2,
       title: "Jayashree Kuwar",
       text: "Social Media Analyst",
     },
 
     {
-      image: DeliveryMeals,
+      image: kalyani2,
       title: "Kalyani Patil",
       text: "Social Media Analyst",
     },
 
     {
-      image: DeliveryMeals,
+      image: akshayblacky,
       title: "Akshay Patil",
       text: "Social Media Manager",
     },
 
     {
-      image: DeliveryMeals,
+      image: amol,
       title: "Amol Patil",
       text: "Legal Advisor",
     },
 
     {
-      image: DeliveryMeals,
+      image: div,
       title: "Divesh Sonawane",
       text: "Field Officer",
     },
     {
-      image: DeliveryMeals,
+      image: bhupesh,
       title: "Bhupesh Patil",
       text: "Field Officer",
     },
     {
-      image: DeliveryMeals,
+      image: Himanshu,
       title: "Himanshu Patil",
       text: "Field Officer",
     },
     {
-      image: DeliveryMeals,
+      image: tejaspadvi,
       title: "Tejas Padvi",
       text: "Field Officer",
     },
     {
-      image: DeliveryMeals,
+      image: sahil,
       title: "Sahil Nagarale",
       text: "Digital Creator",
     },
     {
-      image: DeliveryMeals,
+      image: akshaypatil,
       title: "Akshay Patil",
       text: "Field Officer",
     },
     {
-      image: DeliveryMeals,
+      image: parth,
       title: "Parth Patil",
       text: "Field Officer",
     },
     {
-      image: DeliveryMeals,
+      image: Tejaswini2,
       title: "Tejaswini Kotkar",
       text: "Field Officer",
     },
     {
-      image: DeliveryMeals,
+      image: Vaishali,
       title: "Vaishali Shrirao",
       text: "Digital Creator (YouTube)",
     },
     {
-      image: DeliveryMeals,
+      image: gym,
       title: "Akshay Patil",
       text: "Field Officer",
     },
